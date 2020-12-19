c7eb229da3faac7fed0dd37604fd7ea4a2e7e6bd
Jasmin0904 <75204509+Jasmin0904@users.noreply.github.com>
2020-12-19 15:59:33 +0100

    initial commit
    initial commit

---
+++ src/Portfolio/Controllers/InformationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class InformationController : Controller
+    {
+        // GET: Information
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DynamischeWebseiten()
+        {
+            return View();
+        }
+
+        public ActionResult FaktenASP()
+        {
+            return View();
+        }
+
+        public ActionResult Framework()
+        {
+            return View();
+        }
+
+        public ActionResult MVC()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Portfolio/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Portfolio.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public int Matrikelnummer { get; set; }
+
+        public string Name { get; set; }
+
+        public string Kurs { get; set; }
+    }
+
+    public class StudentDBContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
